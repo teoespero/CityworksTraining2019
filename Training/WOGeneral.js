473dbf54026aeeea0423895c19301c65f3270a98
Teo Espero <38012835+teoespero@users.noreply.github.com>
2019-12-05 17:32:11 -0700

    Add files via upload

---
+++ Training/WOGeneral.js
@@ -0,0 +1,6 @@
+﻿
+// shorten the field size for the Description
+$(function(){
+    document.getElementById('ctl00_Main_cboWODesc').classList.remove('long');
+    document.getElementById('ctl00_Main_cboWODesc').classList.add('normal');
+  });
